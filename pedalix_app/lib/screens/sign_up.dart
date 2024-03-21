428375034e1b3acc33625cf6238a1b748e367a06
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-03-21 15:55:09 +0530

    Implement the google auth for sign up page

--- pedalix_app/lib/screens/sign_up.dart
+++ pedalix_app/lib/screens/sign_up.dart
@@ -4,19 +4,81 @@ import 'package:country_pickers/country_pickers.dart';
 import 'package:pedalix_app/widgets/custom_outlined_button.dart';
 import 'package:flutter/material.dart';
 import 'package:pedalix_app/core/app_export.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   SignUpScreen({Key? key})
       : super(
           key: key,
         );
 
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
   Country selectedCountry = CountryPickerUtils.getCountryByPhoneCode('94');
 
   TextEditingController phoneNumberController = TextEditingController();
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final firestoreInstance = FirebaseFirestore.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event) {
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google SignIn"),
+      ),
+      body: _user != null ? _userInfo() : _signInScreen(),
+    );
+  }
+
+  Widget _userInfo() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(_user!.photoURL!),
+              ),
+            ),
+          ),
+          Text(_user!.email!),
+          Text(_user!.displayName ?? ""),
+          MaterialButton(
+            color: Colors.red,
+            child: const Text("Sign Out"),
+            onPressed: _auth.signOut,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _signInScreen() {
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomInset: false,
@@ -43,7 +105,8 @@ class SignUpScreen extends StatelessWidget {
               _buildOR(context),
               SizedBox(height: 36.v),
               CustomOutlinedButton(
-                text: "Sign  in with Google",
+                text: "Sign in with Google",
+                onPressed: _handleGoogleSignIn,
                 leftIcon: Container(
                   margin: EdgeInsets.only(right: 30.h),
                   child: CustomImageView(
@@ -55,7 +118,7 @@ class SignUpScreen extends StatelessWidget {
               ),
               SizedBox(height: 14.v),
               CustomOutlinedButton(
-                text: "Sign  in with Facebook",
+                text: "Sign in with Facebook",
                 leftIcon: Container(
                   margin: EdgeInsets.only(right: 30.h),
                   child: CustomImageView(
@@ -103,5 +166,14 @@ class SignUpScreen extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void _handleGoogleSignIn() {
+    try {
+      GoogleAuthProvider _googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(_googleAuthProvider);
+    } catch (error) {
+      print(error);
+    }
   }
 }
